8a1c9ecc1d18f96c818ac38ebd7cbdbeb31038e8
MaxVoloskiy <voloskiy@ucu.edu.ua>
2019-03-04 22:29:15 +0200

    refactored ViewPost

--- src/Components/ViewPost/ViewPost.js
+++ src/Components/ViewPost/ViewPost.js
@@ -1,14 +1,23 @@
 import React from "react";
 
-import Post from "./Post/Post";
-
 import "../../css-grid/grid.scss";
 
-const jsonServerUrl = "http://localhost:3001/";
+import "./ViewPost.scss"
 
 const ViewPost = props => {
-  const { data } = props.location.state;
-  return <Post serverUrl={jsonServerUrl} article={data} />;
+  const article  = props.location.state.data;
+  return (
+    <div className={"ViewPost"}>
+      <h1 className={"ViewPost__title"}>{article.title}</h1>
+      <h3 className={"ViewPost__author"}>{article.author}</h3>
+      <p className={"ViewPost__content"}>{article.content}</p>
+      <ul className={"ViewPost__tags"}>
+        {article.tags.map(tag => (
+          <li className={"ViewPost__tags_tag"}>{tag.text}</li>
+        ))}
+      </ul>
+    </div>
+  )
 };
 
 export default ViewPost;
